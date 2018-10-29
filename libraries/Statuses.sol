488ab192577476ddada87cdd4d76b2e30953d20f
Lapaka <42385544+Lapaka@users.noreply.github.com>
2018-10-29 16:48:45 +0100

    Add files via upload

--- libraries/Statuses.sol
+++ libraries/Statuses.sol
@@ -1,1 +1,38 @@
+// Code for Statuses.sol
+
+pragma solidity ^0.4.15;
+
+//
+//  This library contains constant values for both sc: Claim and Attestation
+//  
+
+library Statuses {
+    // Status definition for Claims
+    enum ClaimStatus {Valid, Received, AskDeletion, DeletionConfirmation}
+    
+    int constant C_STATUS_FIRST = int(ClaimStatus.Valid);
+    int constant C_STATUS_LAST = int(ClaimStatus.DeletionConfirmation);
+            
+    function ClaimStatusFirst_get() internal pure returns (int) {         
+      return C_STATUS_FIRST;
+    }
+    
+    function ClaimStatusLast_get() internal pure returns (int) {  
+      return C_STATUS_LAST;
+    }
+    
+    //  Status definitions for Attestations
+    enum AttestationStatus {Valid, AskIssuer, Revoked, DeletedBySubject}
+    
+    int constant A_STATUS_FIRST = int(AttestationStatus.Valid);
+    int constant A_STATUS_LAST = int(AttestationStatus.DeletedBySubject);
+    
+    function AttestationStatusFirst_get() internal pure returns (int) {         
+      return A_STATUS_FIRST;
+    }
+    
+    function AttestationStatusLast_get() internal pure returns (int) {  
+      return A_STATUS_LAST;
+    }
+}
 
